9c1f52125735cabce9120e3f6a0d83f8fe614690
Edi Javier <126507385+edijavier99@users.noreply.github.com>
2023-06-08 21:02:07 +0000

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,32 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+let Counter = (props) =>{
+    return(
+        <div className="general-container d-flex align-items-center justify-content-center vh-100">
+        <div className="icon"><i class="fas fa-clock"></i></div>
+        <div className="one">{props.digitOne %10}</div>
+        <div className="two">{props.digitTwo %10}</div>
+        <div className="three">{props.digitThree %10}</div>
+        <div className="four">{props.digitFour %10}</div>
+        <div className="five">{props.digitFive %10}</div>
+        <div className="six">{props.digitSix %10}</div>
+    </div>
+    )
+   
+};
 
+
+let count =0
+    setInterval(function(){
+            const six = Math.floor(count/1);
+            const five = Math.floor(count/10);
+            const four = Math.floor(count/100);
+            const three = Math.floor(count/10000);
+            const two = Math.floor(count/100000);
+            const one = Math.floor(count/1000000);
+            count ++;
+    
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Counter digitOne={one} digitTwo={two} digitFour={four} digitFive={five} digitThree={three} digitSix={six}/>, document.querySelector("#app"));
+},1000)
